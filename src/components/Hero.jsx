7bb8cfa6413daaaca86debbfc807f798836a1428
santhoshv120505 <santhoshv120505@gmail.com>
2026-03-26 18:55:04 +0530

    updated projects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
             {/* BUTTON 2 — PUBLICATIONS */}
             <a
               href="#publications"
-              className="px-6 py-3 rounded-lg bg-white/10 border-white/30 hover:border-purple-300 transition text-white"
+              className="px-6 py-3 rounded-lg bg-white/10 border-white/30 hover:border-cyan-300 transition text-white"
             >
               View Publications
             </a>
